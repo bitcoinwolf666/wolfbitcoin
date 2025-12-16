b40b78b892999f66d08c0d79f6df19f8f3ddf313
bitcoinwolf666 <zivkamarinkovic@protonmail.com>
2025-12-16 21:48:15 +0000

    Update server.js

--- server.js
+++ server.js
@@ -4,4 +4,9 @@ if (STRIPE_SECRET_KEY) {
 } else {
   console.log("STRIPE_SECRET_KEY not set yet (payments disabled for now).");
 }
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on ${PORT}`);
+});
 
